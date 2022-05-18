9a3a3cac8a1e8364184844196854fa2cadb269f8
dzering <71943051+dzering@users.noreply.github.com>
2022-05-18 23:22:31 +0400

    Create AnaliticsManager.cs

---
+++ Assets/_Root/Scripts/Services/Analitics/AnaliticsManager.cs
@@ -0,0 +1,33 @@
+﻿using MyGame.Services.Analitics.UnityAnalitics;
+using System;
+using UnityEngine;
+
+
+namespace MyGame.Services.Analitics
+{
+    internal class AnaliticsManager : MonoBehaviour 
+    {
+        private IAnaliticsService[] services;
+
+        private void Awake()
+        {
+            services = new IAnaliticsService[]
+            {
+                new UnityAnaliticsService()
+            };
+        }
+
+        public void SendMainMenuOpend()
+        {
+            SendEvent("MainMenuOpend");
+        }
+
+        private void SendEvent(string eventName)
+        {
+            for (int i = 0; i < services.Length; i++)
+            {
+                services[i].SendEvent(eventName);
+            }
+        }
+    }
+}
