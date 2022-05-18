e3e784aee4666ee54a65ed0a4cb6e7c7be2282c3
dzering <71943051+dzering@users.noreply.github.com>
2022-05-18 23:14:28 +0400

    Add class UnitiAnaliticsService

---
+++ Assets/_Root/Scripts/Services/Analitics/UnityAnalitics/UnityAnaliticsService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using MyGame.Services.Analitics;
+
+namespace MyGame.Services.Analitics.UnityAnalitics
+{
+    internal class UnityAnaliticsService : IAnaliticsService
+    {
+        public void SendEvent(string alias)
+        {
+            UnityEngine.Analytics.Analytics.CustomEvent(alias);
+        }
+
+        public void SendEvent(string alias, Dictionary<string, object> eventData)
+        {
+            UnityEngine.Analytics.Analytics.CustomEvent(alias, eventData);
+        }
+    }
+}
