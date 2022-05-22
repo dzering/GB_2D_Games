556f696d24e7721f05d9553d9e81339c5def3df7
dzering <71943051+dzering@users.noreply.github.com>
2022-05-22 23:13:17 +0400

    Created Repository

---
+++ Assets/_Root/Scripts/Repository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+    internal interface IRepository : IDisposable
+    {
+
+    }
+
+
+    internal abstract class Repository<Tkey, TValue, TConfig> : IRepository
+    {
+        protected Dictionary<Tkey, TValue> items;
+
+        public IReadOnlyDictionary<Tkey, TValue> Items => items;
+
+        public Repository(IEnumerable<TConfig> configs)
+        {
+            items = CreateRepository(configs);
+        }
+
+        private Dictionary<Tkey, TValue> CreateRepository(IEnumerable<TConfig> configs)
+        {
+            Dictionary<Tkey, TValue> items = new Dictionary<Tkey, TValue>();
+
+            foreach (var config in configs)
+            {
+                items[GetKey()] = CreateItem(config);
+            }
+
+            return items;
+        }
+
+        protected abstract TValue CreateItem(TConfig config);
+
+        protected abstract Tkey GetKey();
+
+
+        public void Dispose()
+        {
+            items.Clear();
+        }
+    }
+}
